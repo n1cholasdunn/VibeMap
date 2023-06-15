1a47bd26795ea897404032f5cff966235e848752
Nicholas J Dunn <nicholasduunn@gmail.com>
2023-06-15 08:28:34 -0700

    converted to arrow func

--- client/src/components/map/CategorySearch.js
+++ client/src/components/map/CategorySearch.js
@@ -5,74 +5,65 @@ import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
 import CheckBoxIcon from '@mui/icons-material/CheckBox';
 import { useEffect } from 'react';
 
-const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
-const checkedIcon = <CheckBoxIcon fontSize="small" />;
+const icon = <CheckBoxOutlineBlankIcon fontSize='small' />;
+const checkedIcon = <CheckBoxIcon fontSize='small' />;
+const CategorySearch = ({ setSelectedCategories, selectedCategories }) => {
+  const handleCategories = (event, value) => {
+    console.log('category changed');
+    console.log('value ==> ', value);
+    setSelectedCategories(value);
+  };
+  useEffect(() => {
+    console.log('categoy sel:', selectedCategories);
+  }, [selectedCategories]);
 
-function CategorySearch({ setSelectedCategories, selectedCategories }) {
-
-    const handleCategories = (event, value) => {
-        console.log('category changed')
-        console.log('value ==> ', value)
-        setSelectedCategories(value)
-
-    }
-    useEffect(() => {
-        console.log('categoy sel:', selectedCategories)
-
-    }, [selectedCategories])
-
-
-    return (
-
-        <Autocomplete
-            multiple
-            options={categories}
-            disableCloseOnSelect
-            getOptionLabel={(option) => option.categoryName}
-            renderOption={(props, option, { selected }) => (
-                <li {...props}>
-                    <Checkbox
-                        icon={icon}
-                        checkedIcon={checkedIcon}
-                        style={{ marginRight: 8, }}
-                        className='z-0 '
-                        checked={selected}
-                    />
-                    {option.categoryName}
-                </li>
-            )}
-            style={{ width: 500, }}
-            value={selectedCategories}
-            onChange={handleCategories}
-
-            renderInput={(params) => (
-
-                <TextField {...params} label="Your vibe is..." placeholder="Interests" />
-
-            )}
-
+  return (
+    <Autocomplete
+      multiple
+      options={categories}
+      disableCloseOnSelect
+      getOptionLabel={(option) => option.categoryName}
+      renderOption={(props, option, { selected }) => (
+        <li {...props}>
+          <Checkbox
+            icon={icon}
+            checkedIcon={checkedIcon}
+            style={{ marginRight: 8 }}
+            className='z-0 '
+            checked={selected}
+          />
+          {option.categoryName}
+        </li>
+      )}
+      style={{ width: 500 }}
+      value={selectedCategories}
+      onChange={handleCategories}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label='Your vibe is...'
+          placeholder='Interests'
         />
-    );
-}
+      )}
+    />
+  );
+};
 
 const categories = [
-    { "categoryName": "Adventure" },
-    { "categoryName": "Architecture" },
-    { "categoryName": "Art" },
-    { "categoryName": "Beaches" },
-    { "categoryName": "Cuisine" },
-    { "categoryName": "History" },
-    { "categoryName": "Nature" },
-    { "categoryName": "Nightlife" },
-    { "categoryName": "Parties" },
-    { "categoryName": "Relaxation" },
-    { "categoryName": "Shopping" },
-    { "categoryName": "Sports" },
-    { "categoryName": "Wine" },
-    { "categoryName": "Wildlife" }
-]
+  { categoryName: 'Adventure' },
+  { categoryName: 'Architecture' },
+  { categoryName: 'Art' },
+  { categoryName: 'Beaches' },
+  { categoryName: 'Cuisine' },
+  { categoryName: 'History' },
+  { categoryName: 'Nature' },
+  { categoryName: 'Nightlife' },
+  { categoryName: 'Parties' },
+  { categoryName: 'Relaxation' },
+  { categoryName: 'Shopping' },
+  { categoryName: 'Sports' },
+  { categoryName: 'Wine' },
+  { categoryName: 'Wildlife' },
+];
 
-
-
-
-export default CategorySearch
+export default CategorySearch;
