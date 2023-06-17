ed13db39003d4722c20986557eb53085351f1a9c
Nicholas J Dunn <nicholasduunn@gmail.com>
2023-06-17 13:46:28 -0700

    cypress installed and tried fixing locationpointcontroller

--- server/controllers/locationPointController.ts
+++ server/controllers/locationPointController.ts
@@ -1,53 +1,56 @@
 import sequelize from '../models/index';
+import { Model } from 'sequelize';
 import { Request, Response } from 'express';
-const LocationPoint = sequelize.models.LocationPoint;
+import LocationPoint, {
+  LocationPointAttributes,
+} from '../models/locationPointModel';
+const LocationPointModel = sequelize.models.LocationPoint;
 //updated
 
 interface Trip {
-    start: string;
-    midpoint: string;
-    end: string;
+  start: string;
+  midpoint: string;
+  end: string;
 }
 
 export const postLocationPoint = async (req: Request, res: Response) => {
-    const { type, start, midpoint, end } = req.body;
-    try {
-        const trip = await LocationPoint.create({
-            type,
-            start: JSON.stringify(start),
-            midpoint: JSON.stringify(midpoint),
-            end: JSON.stringify(end),
-        });
-        res.status(201).json(trip);
-    } catch (err) {
-        let messageErr;
-        if (err instanceof Error) messageErr = err.message;
-        else messageErr = String(err);
-        res.status(400).json({ message: messageErr });
-    }
+  const { type, start, midpoint, end } = req.body;
+  try {
+    const trip = await LocationPointModel.create({
+      type,
+      start: JSON.stringify(start),
+      midpoint: JSON.stringify(midpoint),
+      end: JSON.stringify(end),
+    });
+    res.status(201).json(trip);
+  } catch (err) {
+    let messageErr;
+    if (err instanceof Error) messageErr = err.message;
+    else messageErr = String(err);
+    res.status(400).json({ message: messageErr });
+  }
 };
 interface tripTypes {
-  type: string;
+  type?: string;
   start: string;
   midpoint: string;
   end: string;
 }
 
 export const getLocationPoint = async (req: Request, res: Response) => {
-    try {
-        const trips = await LocationPoint.findAll({});
-        trips.forEach((trip) => {
-            // trips.forEach((trip: { start: string; midpoint: string; end: string }) => {
-            (trip.start) = JSON.parse(trip.start);
-            trip.midpoint = JSON.parse(trip.midpoint);
-            trip.end = JSON.parse(trip.end);
-        });
-        res.json(trips);
-    } catch (err) {
-        let messageErr;
-        if (err instanceof Error) messageErr = err.message;
-        else messageErr = String(err);
-        res.status(400).json({ message: messageErr });
-    }
+  try {
+    const trips: any = await LocationPointModel.findAll({});
+    trips.forEach((trip: Trip) => {
+      //   trips.forEach((trip: { start: string; midpoint: string; end: string }) => {
+      trip.start = JSON.parse(trip.start);
+      trip.midpoint = JSON.parse(trip.midpoint);
+      trip.end = JSON.parse(trip.end);
+    });
+    res.json(trips);
+  } catch (err) {
+    let messageErr;
+    if (err instanceof Error) messageErr = err.message;
+    else messageErr = String(err);
+    res.status(400).json({ message: messageErr });
+  }
 };
-
--- server/models/locationPointModel.ts
+++ server/models/locationPointModel.ts
@@ -3,11 +3,14 @@ import sequelize from './index';
 
 export interface LocationPointAttributes {
   id: string;
-  name: string;
-  lat: string;
-  lng: string;
-  categories: string;
-  address: string;
+  name?: string;
+  lat?: string;
+  lng?: string;
+  categories?: string;
+  address?: string;
+  start: string;
+  midpoint: string;
+  endpoint: string;
 }
 
 class LocationPoint
@@ -20,6 +23,9 @@ class LocationPoint
   public lng!: string;
   public categories!: string;
   public address!: string;
+  public start!: string;
+  public midpoint!: string;
+  public endpoint!: string;
 
   // Timestamps in db necessary?
   public readonly createdAt!: Date;
@@ -54,6 +60,21 @@ LocationPoint.init(
       allowNull: false,
     },
     address: {
+      type: DataTypes.STRING,
+      primaryKey: false,
+      allowNull: false,
+    },
+    start: {
+      type: DataTypes.STRING,
+      primaryKey: false,
+      allowNull: false,
+    },
+    midpoint: {
+      type: DataTypes.STRING,
+      primaryKey: false,
+      allowNull: false,
+    },
+    endpoint: {
       type: DataTypes.STRING,
       primaryKey: false,
       allowNull: false,
