4125dff34896dc1f3941642b07c586a5fac35505
Tornike <tornike_kutaisi@yahoo.ca>
2023-06-20 12:07:58 -0400

    more error fixes

--- client/src/components/map/TripSearch.tsx
+++ client/src/components/map/TripSearch.tsx
@@ -1,10 +1,10 @@
-import { useRef, useState, useEffect, useContext, FormEvent } from 'react';
-import { Autocomplete } from '@react-google-maps/api';
-import { DestinationContext } from '../../context';
-import { useNavigate } from 'react-router-dom';
-import React from 'react';
-import { Destination } from '../../context';
-import { AutoComplete, Place } from '../../Pages/CreateTrip';
+import { useRef, useState, useEffect, useContext, FormEvent } from "react";
+import { Autocomplete } from "@react-google-maps/api";
+import { DestinationContext } from "../../context";
+import { useNavigate } from "react-router-dom";
+import React from "react";
+import { Destination } from "../../context";
+import { AutoComplete, Place } from "../../Pages/CreateTrip";
 
 interface TripSearchProps {
   selectedTripOption: string;
@@ -23,8 +23,8 @@ const TripSearch: React.FC<TripSearchProps> = ({ selectedTripOption }) => {
 
   useEffect(() => {
     console.log(apiKey);
-    const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`;
+    const script = document.createElement("script");
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places&callback=Function.prototype`;
     script.onload = () => {
       setGoogleLoaded(true);
     };
@@ -38,9 +38,9 @@ const TripSearch: React.FC<TripSearchProps> = ({ selectedTripOption }) => {
   const handlePlaceSelect = (place: Place, name: string) => {
     const { geometry } = place;
     if (geometry) {
-      console.log('geometry', geometry);
-      console.log('place', place);
-      console.log('name', name);
+      console.log("geometry", geometry);
+      console.log("place", place);
+      console.log("name", name);
 
       if (geometry.location) {
         const { lat, lng } = geometry.location;
@@ -65,8 +65,8 @@ const TripSearch: React.FC<TripSearchProps> = ({ selectedTripOption }) => {
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    console.log('tripsearch', destination);
-    navigate('/create');
+    console.log("tripsearch", destination);
+    navigate("/create");
   };
 
   return (
@@ -74,31 +74,33 @@ const TripSearch: React.FC<TripSearchProps> = ({ selectedTripOption }) => {
       {/* -------------------------- ONE WAY TRIP ---------------------------------- */}
       {googleLoaded && (
         <>
-          {selectedTripOption === 'oneWay' && (
-            <div className='flex flex-col items-center justify-center h-72 '>
+          {selectedTripOption === "oneWay" && (
+            <div className="flex flex-col items-center justify-center h-72 ">
               For one-way trips, enter your start and end coords.
               <form
                 onSubmit={handleSubmit}
-                className='flex flex-col items-center justify-center'>
+                className="flex flex-col items-center justify-center"
+              >
                 <div>
-                  <div className='relative'>
+                  <div className="relative">
                     <Autocomplete
                       // apiKey={apiKey} MIGHT BE UNNECESSARY
                       onLoad={(autocomplete) => {
                         autocompleteRefA.current = autocomplete;
-                        autocomplete.setFields(['geometry']);
+                        autocomplete.setFields(["geometry"]);
                       }}
                       onPlaceChanged={() =>
                         handlePlaceSelect(
                           autocompleteRefA.current.getPlace(),
-                          'start'
+                          "start"
                         )
                       }
-                      className='z-[100000]'>
+                      className="z-[100000]"
+                    >
                       <input
-                        placeholder='Start'
-                        className='border border-gray-500 m-2.5 pl-2 '
-                        name='start'
+                        placeholder="Start"
+                        className="border border-gray-500 m-2.5 pl-2 "
+                        name="start"
                         value={
                           destination.coords.start
                             ? destination.coords.start.formatted_address
@@ -112,19 +114,20 @@ const TripSearch: React.FC<TripSearchProps> = ({ selectedTripOption }) => {
                     // apiKey={apiKey} MIGHT BE UNNECESSARY
                     onLoad={(autocomplete) => {
                       autocompleteRefB.current = autocomplete;
-                      autocomplete.setFields(['geometry']);
+                      autocomplete.setFields(["geometry"]);
                     }}
                     onPlaceChanged={() =>
                       handlePlaceSelect(
                         autocompleteRefB.current.getPlace(),
-                        'end'
+                        "end"
                       )
                     }
-                    className='z-[100000]'>
+                    className="z-[100000]"
+                  >
                     <input
-                      placeholder='End'
-                      className='border border-gray-500 m-2.5 pl-2'
-                      name='end'
+                      placeholder="End"
+                      className="border border-gray-500 m-2.5 pl-2"
+                      name="end"
                       value={
                         destination.coords.end
                           ? destination.coords.end.formatted_address
@@ -134,9 +137,10 @@ const TripSearch: React.FC<TripSearchProps> = ({ selectedTripOption }) => {
                   </Autocomplete>
                 </div>
                 <button
-                  type='submit'
-                  className='	hover:text-white text-white hover:bg-gray-500 bg-gray-600 hover:drop-shadow-lg
-									text-md font-semibold p-2 rounded-lg text-sm border border-gray-300  w-28 h-full mt-5 py-2 px-4 '>
+                  type="submit"
+                  className="	hover:text-white text-white hover:bg-gray-500 bg-gray-600 hover:drop-shadow-lg
+									text-md font-semibold p-2 rounded-lg text-sm border border-gray-300  w-28 h-full mt-5 py-2 px-4 "
+                >
                   Go!
                 </button>
               </form>
@@ -145,29 +149,31 @@ const TripSearch: React.FC<TripSearchProps> = ({ selectedTripOption }) => {
 
           {/* -------------------------- SINGLE DESTINATION ---------------------------------- */}
 
-          {selectedTripOption === 'singleDestination' && (
-            <div className='flex flex-col items-center justify-center h-72 '>
+          {selectedTripOption === "singleDestination" && (
+            <div className="flex flex-col items-center justify-center h-72 ">
               For a single destination, enter the main location you want to
               visit.
               <form
                 onSubmit={handleSubmit}
-                className='flex flex-col items-center justify-center'>
+                className="flex flex-col items-center justify-center"
+              >
                 <Autocomplete
                   // apiKey={apiKey} MIGHT BE UNNECESSARY
                   onLoad={(autocomplete) => {
                     autocompleteRefA.current = autocomplete;
-                    autocomplete.setFields(['geometry']);
+                    autocomplete.setFields(["geometry"]);
                   }}
                   onPlaceChanged={() =>
                     handlePlaceSelect(
                       autocompleteRefA.current.getPlace(),
-                      'start'
+                      "start"
                     )
-                  }>
+                  }
+                >
                   <input
-                    placeholder='Destination'
-                    className='border border-gray-500 m-2.5 pl-2'
-                    name='start'
+                    placeholder="Destination"
+                    className="border border-gray-500 m-2.5 pl-2"
+                    name="start"
                     value={
                       destination.coords.start
                         ? destination.coords.start.formatted_address
@@ -176,39 +182,42 @@ const TripSearch: React.FC<TripSearchProps> = ({ selectedTripOption }) => {
                   />
                 </Autocomplete>
                 <button
-                  type='submit'
-                  className='	hover:text-white text-white hover:bg-gray-500 bg-gray-600 hover:drop-shadow-lg
-									text-md font-semibold p-2 rounded-lg text-sm border border-gray-300  w-28 h-full mt-5 py-2 px-4 '>
+                  type="submit"
+                  className="	hover:text-white text-white hover:bg-gray-500 bg-gray-600 hover:drop-shadow-lg
+									text-md font-semibold p-2 rounded-lg text-sm border border-gray-300  w-28 h-full mt-5 py-2 px-4 "
+                >
                   Go!
                 </button>
               </form>
             </div>
           )}
           {/* -------------------------- LOOP TRIP ---------------------------------- */}
-          {selectedTripOption === 'loopTrip' && (
-            <div className='flex flex-col items-center justify-center h-72'>
+          {selectedTripOption === "loopTrip" && (
+            <div className="flex flex-col items-center justify-center h-72">
               For round trips, enter the start and end coords, and the name of a
               place half way down.
               <form
                 onSubmit={handleSubmit}
-                className='flex flex-col items-center justify-center'>
+                className="flex flex-col items-center justify-center"
+              >
                 <div>
                   <Autocomplete
                     // apiKey={apiKey} MIGHT BE UNNECESSARY
                     onLoad={(autocomplete) => {
                       autocompleteRefA.current = autocomplete;
-                      autocomplete.setFields(['geometry']);
+                      autocomplete.setFields(["geometry"]);
                     }}
                     onPlaceChanged={() =>
                       handlePlaceSelect(
                         autocompleteRefA.current.getPlace(),
-                        'start'
+                        "start"
                       )
-                    }>
+                    }
+                  >
                     <input
-                      placeholder='Start'
-                      className='border border-gray-500 m-2.5 pl-2'
-                      name='start'
+                      placeholder="Start"
+                      className="border border-gray-500 m-2.5 pl-2"
+                      name="start"
                       value={
                         destination.coords.start
                           ? destination.coords.start.formatted_address
@@ -220,18 +229,19 @@ const TripSearch: React.FC<TripSearchProps> = ({ selectedTripOption }) => {
                     // apiKey={apiKey} MIGHT BE UNNECESSARY
                     onLoad={(autocomplete) => {
                       autocompleteRefB.current = autocomplete;
-                      autocomplete.setFields(['geometry']);
+                      autocomplete.setFields(["geometry"]);
                     }}
                     onPlaceChanged={() =>
                       handlePlaceSelect(
                         autocompleteRefB.current.getPlace(),
-                        'midpoint'
+                        "midpoint"
                       )
-                    }>
+                    }
+                  >
                     <input
-                      placeholder='Midpoint'
-                      className='border border-gray-500 m-2.5 pl-2'
-                      name='midpoint'
+                      placeholder="Midpoint"
+                      className="border border-gray-500 m-2.5 pl-2"
+                      name="midpoint"
                       value={
                         destination.coords.midpoint
                           ? destination.coords.midpoint.formatted_address
@@ -243,18 +253,19 @@ const TripSearch: React.FC<TripSearchProps> = ({ selectedTripOption }) => {
                     // apiKey={apiKey} MIGHT BE UNNECESSARY
                     onLoad={(autocomplete) => {
                       autocompleteRefC.current = autocomplete;
-                      autocomplete.setFields(['geometry']);
+                      autocomplete.setFields(["geometry"]);
                     }}
                     onPlaceChanged={() =>
                       handlePlaceSelect(
                         autocompleteRefC.current.getPlace(),
-                        'end'
+                        "end"
                       )
-                    }>
+                    }
+                  >
                     <input
-                      placeholder='End'
-                      className='border border-gray-500 m-2.5 pl-2'
-                      name='end'
+                      placeholder="End"
+                      className="border border-gray-500 m-2.5 pl-2"
+                      name="end"
                       value={
                         destination.coords.end
                           ? destination.coords.end.formatted_address
@@ -264,9 +275,10 @@ const TripSearch: React.FC<TripSearchProps> = ({ selectedTripOption }) => {
                   </Autocomplete>
                 </div>
                 <button
-                  type='submit'
-                  className='	hover:text-white text-white hover:bg-gray-500 bg-gray-600 hover:drop-shadow-lg
-									text-md font-semibold p-2 rounded-lg text-sm border border-gray-300  w-28 h-full mt-5 py-2 px-4 '>
+                  type="submit"
+                  className="	hover:text-white text-white hover:bg-gray-500 bg-gray-600 hover:drop-shadow-lg
+									text-md font-semibold p-2 rounded-lg text-sm border border-gray-300  w-28 h-full mt-5 py-2 px-4 "
+                >
                   Go!
                 </button>
               </form>
